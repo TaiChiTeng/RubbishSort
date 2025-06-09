eb72f8beaa437d3d9d2f0dc947dc04bd4902f6e1
ArTenGo <taichi.teng@gmail.com>
2025-06-09 09:39:19 +0800

    Bucket换Bin；GameManager存4个垃圾箱和点击3个按钮交换垃圾箱位置

--- GameManager.ts
+++ GameManager.ts
@@ -1,5 +1,7 @@
-import { _decorator, Component, Node, Label, director } from 'cc';
+import { _decorator, Component, Node, Label, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
+
+const GAME_TIME = 5; // 定义全局游戏时间，单位为秒，测试版用5秒，正式版60秒吧
 
 @ccclass('GameManager')
 export class GameManager extends Component {
@@ -22,8 +24,14 @@ export class GameManager extends Component {
     @property({ type: Label })
     public finalGameScoreLabel: Label = null;
 
-    // 一局游戏的时间，单位为秒，临时用3秒测试，后续改用60秒
-    private _countDownTime: number = 3;
+    @property({ type: [Node] })
+    public Bins: Node[] = [];  // 存储4个垃圾箱节点
+
+    @property({ type: [Node] })
+    public BinPos: Node[] = []; // 存储4个垃圾箱节点的初始坐标节点
+
+    // 一局游戏的时间，单位为秒
+    private _countDownTime: number = GAME_TIME;
     private _isCounting: boolean = false;
     private _gameScore: number = 0;
 
@@ -32,6 +40,9 @@ export class GameManager extends Component {
         this.mainMenu.active = true;
         this.gamePlay.active = false;
         this.timeOver.active = false;
+
+        // 初始化垃圾箱位置
+        this.initBinsPosition();
     }
 
     update(deltaTime: number) {
@@ -74,10 +85,21 @@ export class GameManager extends Component {
     private initGameData() {
         // 初始化分数
         this._gameScore = 0;
+        this.updateScoreLabel();
         // 初始化倒计时
-        this._countDownTime = 10;
+        this._countDownTime = GAME_TIME;
         this.countDownLabel.string = this._countDownTime.toString();
         this._isCounting = true;
+
+        // 重新初始化垃圾箱位置
+        this.initBinsPosition();
+    }
+
+    // 初始化垃圾箱位置
+    private initBinsPosition() {
+        for (let i = 0; i < this.Bins.length; i++) {
+            this.Bins[i].setPosition(this.BinPos[i].position.x, this.BinPos[i].position.y, 0);
+        }
     }
 
     // 时间结束的处理函数
@@ -102,5 +124,32 @@ export class GameManager extends Component {
     // 更新分数Label显示
     private updateScoreLabel() {
         this.gameScoreLabel.string = this._gameScore.toString();
+    }
+
+    // 点击左边按钮
+    public onLeftButtonClicked() {
+        this.swapBins(0, 1);
+    }
+
+    // 点击中间按钮
+    public onMiddleButtonClicked() {
+        this.swapBins(1, 2);
+    }
+
+    // 点击右边按钮
+    public onRightButtonClicked() {
+        this.swapBins(2, 3);
+    }
+
+    // 交换垃圾箱
+    private swapBins(index1: number, index2: number) {
+        // 交换节点
+        let temp = this.Bins[index1];
+        this.Bins[index1] = this.Bins[index2];
+        this.Bins[index2] = temp;
+
+        // 交换位置
+        this.Bins[index1].setPosition(this.BinPos[index1].position.x, this.BinPos[index1].position.y, 0);
+        this.Bins[index2].setPosition(this.BinPos[index2].position.x, this.BinPos[index2].position.y, 0);
     }
 }
